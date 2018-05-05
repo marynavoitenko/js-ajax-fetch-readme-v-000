96e4e59a3619b59841f46547b1f87a09b6d269b2
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-05-05 15:19:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = 'c02c83266e6bdd77a81f746511eb21ac909dcb13';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
